5c8da2ff365511bd79e83f0129c224f7917863db
Sandra <sandra.gufler@gmx.ch>
2024-05-30 18:26:40 +0200

    fix: remove test form angular json

--- karma.config.js
+++ karma.config.js
@@ -1,67 +1,53 @@
 module.exports = function(config) {
   config.set({
-    // Base path that will be used to resolve all patterns (e.g. files, exclude)
     basePath: '',
-
-    // Frameworks to use
-    frameworks: ['jasmine'], // or any other testing framework you are using
-
-    // List of files / patterns to load in the browser
+    frameworks: ['jasmine'],
     files: [
-      'src/**/*.spec.js' // adjust the pattern to your spec files location
+      'src/**/*.spec.js'
     ],
-
-    // List of files / patterns to exclude
     exclude: [],
-
-    // Preprocess matching files before serving them to the browser
     preprocessors: {
-      'src/**/*.spec.js': ['webpack'] // adjust the pattern to your spec files location
+      'src/**/*.spec.js': ['webpack']
     },
-
-    // Webpack configuration
     webpack: {
-      // your webpack configuration
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
     },
-
-    // Test results reporter to use
-    reporters: ['progress'], // add more reporters if needed
-
-    // Web server port
+    reporters: ['progress'],
     port: 9876,
-
-    // Enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // Level of logging
-    logLevel: config.LOG_INFO,
-
-    // Enable / disable watching file and executing tests whenever any file changes
+    logLevel: config.LOG_DEBUG, // Set log level to DEBUG for detailed logs
     autoWatch: false,
-
-    // Start these browsers
     browsers: ['ChromeHeadlessNoSandbox'],
-
-    // Custom launcher for ChromeHeadless with no sandbox
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
-
-    // Continuous Integration mode
     singleRun: true,
-
-    // Concurrency level
     concurrency: Infinity,
-
-    // Plugins to load
+    browserNoActivityTimeout: 60000,
+    captureTimeout: 120000,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 2,
     plugins: [
       'karma-jasmine',
       'karma-chrome-launcher',
       'karma-webpack',
-      'karma-progress-reporter' // ensure to include all the necessary plugins
+      'karma-progress-reporter'
     ]
   });
 };
